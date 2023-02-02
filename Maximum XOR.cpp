6dc39487a2c29c9dfd9f98913c1123407fc121c6
Nayemur Rahman Nayem <48326626+NayemurRahman-Nayem@users.noreply.github.com>
2023-02-02 22:26:23 +0600

    Create Maximum XOR.cpp

---
+++ Maximum XOR.cpp
@@ -0,0 +1,66 @@
+
+#include<bits/stdc++.h> 
+using namespace std ; 
+
+
+struct Node {
+    Node * links[2] ;
+    bool containsKey(int bit) {
+        return (links[bit]!=NULL)  ; 
+    }
+    Node * get(int bit) {
+        return links[bit] ; 
+    }
+    void put(int bit, Node * node) {
+        links[bit] = node ; 
+    }
+}; 
+
+
+class Trie {
+    private: Node * root ; 
+    public: 
+    Trie() {
+        root = new Node() ; 
+    }
+    public:
+    void insert(int num) {
+        Node * node = root ; 
+        for(int i=31;i>=0;i--) {
+            int bit = (num >> i) & 1;  
+            if(!node->containsKey(bit)) {
+                node->put(bit,new Node()) ; 
+            } 
+            node = node->get(bit) ; 
+        }
+    }
+    public:
+    int findMax(int num) {
+        Node * node = root ; 
+        int maxNum = 0 ; 
+        for(int i=31;i>=0;i--) {
+            int bit = (num >> i) & 1; 
+            if(node->containsKey(!bit)) {
+                maxNum = maxNum | (1<<i) ; 
+                node = node->get(!bit) ;  
+            }
+            else {
+                node = node->get(bit) ; 
+            }
+        }
+        return maxNum ; 
+    }
+};
+
+int maxXOR(int n, int m, vector<int> &arr1, vector<int> &arr2) 
+{
+    Trie T ; 
+    int ans = 0 ; 
+    for(auto it:arr1) {
+        T.insert(it) ; 
+    }
+    for(auto it:arr2) {
+        ans = max(ans,T.findMax(it)) ; 
+    }
+    return ans ; 
+}
